fb5d95591cb37d8ec48610d252d5ab6fbf149289
David Teller <david.teller@pasqal.com>
2024-11-04 18:13:17 +0100

    Well, it seems to work

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,220 @@
-use std::path::{Path, PathBuf};
+use std::{collections::HashSet, fmt::Display, path::PathBuf, time::Duration};
 
-use serde::Deserialize;
+use chrono::Timelike;
+use lazy_regex::lazy_regex;
+use serde::{
+    de::{Unexpected, Visitor},
+    Deserialize, Serialize,
+};
+use validator::{Validate, ValidationError};
 
-#[derive(Deserialize)]
-struct Interval {
-    
+#[derive(PartialEq, Eq, Hash, Debug, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Binary(pub PathBuf);
+
+impl Display for Binary {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
 }
 
-#[derive(Deserialize)]
-struct Watch {
-    binary: PathBuf,
-    permitted: Vec<Interval>,
+#[derive(PartialEq, Eq, Debug, Serialize, Clone, Copy)]
+pub struct TimeOfDay {
+    pub hours: u8,
+    pub minutes: u8,
+}
+impl From<TimeOfDay> for std::time::Duration {
+    fn from(t: TimeOfDay) -> std::time::Duration {
+        std::time::Duration::new(t.hours as u64 * 3_600 + t.minutes as u64 * 60, 0)
+    }
+}
+impl<Tz: chrono::TimeZone> From<chrono::DateTime<Tz>> for TimeOfDay {
+    fn from(value: chrono::DateTime<Tz>) -> Self {
+        TimeOfDay {
+            hours: value.hour() as u8,
+            minutes: value.minute() as u8,
+        }
+    }
+}
+impl PartialOrd for TimeOfDay {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for TimeOfDay {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.hours.cmp(&other.hours).then_with(||self.minutes.cmp(&other.minutes))
+    }
 }
 
-#[derive(Deserialize)]
-struct Config {
+impl<'de> Deserialize<'de> for TimeOfDay {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct StrVisitor;
+        impl<'d> Visitor<'d> for StrVisitor {
+            type Value = TimeOfDay;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str(
+                    "a time of day in format HHMM, e.g. \"1135\" (11:35 am) or \"1759\" (5:59pm)",
+                )
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                if v.len() != 4 {
+                    return Err(E::invalid_length(v.len(), &"4"));
+                }
+                let re = lazy_regex!("([0-2][0-9])([0-5][0-9])");
+                let Some(captures) = re.captures(v) else {
+                    return Err(E::invalid_value(
+                        Unexpected::Str(v),
+                        &"a time of day, e.g. \"1135\" (11:35 am) or \"1759\" (5:59pm)",
+                    ));
+                };
+                let (_, [hh, mm]) = captures.extract();
+                let Ok(h) = hh.parse::<u8>() else {
+                    return Err(E::invalid_value(
+                        Unexpected::Str(hh),
+                        &"a number between 00 and 23",
+                    ));
+                };
+                let Ok(m) = mm.parse::<u8>() else {
+                    return Err(E::invalid_value(
+                        Unexpected::Str(mm),
+                        &"a number between 00 and 59",
+                    ));
+                };
+                // Validate.
+                match (h, m) {
+                    (24, 00) => {},
+                    _ if h > 23 => {
+                        return Err(E::invalid_value(
+                            Unexpected::Str(hh),
+                            &"a number between 00 and 23",
+                        ));    
+                    },
+                    _ if m > 59 => {
+                        return Err(E::invalid_value(
+                            Unexpected::Str(mm),
+                            &"a number between 00 and 59",
+                        ));
+                    }
+                    _ => {}
+                }
+
+                Ok(TimeOfDay {
+                    hours: h,
+                    minutes: m,
+                })
+            }
+        }
+        deserializer.deserialize_str(StrVisitor)
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Interval {
+    pub start: TimeOfDay,
+    pub end: TimeOfDay,
+}
+impl Interval {
+    pub fn remaining(&self, time: TimeOfDay) -> Option<std::time::Duration> {
+        if self.start > time || self.end < time {
+            return None
+        }
+        let end: Duration = self.end.into();
+        let time: Duration = time.into();
+        Some(end - time)
+    }
+}
+
+#[derive(Deserialize, Serialize, Validate)]
+pub struct Watch {
+    pub user: String,
+    pub binary: Binary,
+
+    // FIXME: Validate that there are no intersections between intervals.
+    pub permitted: Vec<Interval>,
+}
+
+#[derive(Deserialize, Serialize, Validate)]
+pub struct Config {
     /// The list of processes to watch and kill.
-    watch: Vec<Watch>
+    #[validate(custom(function=Config::validate_distinct_binaries))]
+    pub watch: Vec<Watch>,
+}
+impl Config {
+    fn validate_distinct_binaries(watch: &[Watch]) -> Result<(), ValidationError> {
+        let mut set = HashSet::new();
+        for watch in watch {
+            if !set.insert(&watch.binary) {
+                let mut error = ValidationError::new("duplicate binary");
+                error.add_param("binary".into(), &watch.binary);
+                return Err(error);
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::config::TimeOfDay;
+
+    use super::Config;
+
+    #[test]
+    fn test_config_good() {
+        let sample = r#"
+        watch:
+            - binary: /bin/test
+              user: donald
+              permitted:
+                - start: 0911
+                  end: 0923
+            - binary: /bin/test2
+              user: duck
+              permitted:
+                - start: 0000
+                  end:   0001
+                - start: 0002
+                  end:   0003
+        "#;
+        let config: Config = serde_yaml::from_str(sample).expect("invalid config");
+        assert_eq!(config.watch.len(), 2);
+        assert_eq!(config.watch[0].binary.0.to_string_lossy(), "/bin/test");
+        assert_eq!(config.watch[1].binary.0.to_string_lossy(), "/bin/test2");
+        assert_eq!(
+            config.watch[1].permitted[1].start,
+            TimeOfDay {
+                hours: 0,
+                minutes: 2
+            }
+        );
+    }
+
+
+    #[test]
+    fn test_config_bad_interval() {
+        let sample = r#"
+        watch:
+            - binary: /bin/test
+              permitted:
+                - start: 0911
+                  end: 0923
+            - binary: /bin/test2
+              permitted:
+                - start: 0000
+                  end:   0001
+                - start: 0002
+                  end:   2403
+        "#;
+        assert!(serde_yaml::from_str::<Config>(sample).is_err());
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,158 @@
+use std::{collections::HashMap, path::Path, thread};
+
+use anyhow::Context;
+use clap::Parser;
+use config::{Binary, TimeOfDay};
+use log::info;
+use notify::notify;
+use procfs::ProcError;
+use systemd_journal_logger::{connected_to_journal, JournalLog};
+use uid_resolver::Uid;
+
 mod config;
 mod notify;
+mod uid_resolver;
 
-#[tokio::main]
-pub async fn main() -> Result<(), anyhow::Error> {
-    notify::notify("test message", notify::Urgency::Critical, std::time::Duration::from_millis(10_000)).await?;
+const CONFIG_PATH: &str = "/etc/keep-it-focused.yaml";
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// How often to check for offending processes.
+    #[arg(short, long, default_value="60")]
+    sleep_s: u64,
+}
+
+pub fn main() -> Result<(), anyhow::Error> {
+    if connected_to_journal() {
+        JournalLog::new().unwrap().install().unwrap();
+    } else {
+        simple_logger::SimpleLogger::new().env().init().unwrap();
+    }
+    let args = Args::parse();
+
+    let mut last_modified = std::fs::metadata(CONFIG_PATH)
+        .context("could not find configuration")?
+        .modified()
+        .context("no latest modification time")?;
+    let mut config = init(&CONFIG_PATH)
+        .context("could not read configuration")?;
+
+    loop {
+        info!("sleeping");
+        thread::sleep(std::time::Duration::from_secs(args.sleep_s));
+        let modified = std::fs::metadata(CONFIG_PATH)
+        .context("could not find configuration")?
+        .modified()
+        .context("no latest modification time")?;
+        if modified > last_modified {
+            info!("reloading config");
+            last_modified = modified;
+            config = init(&CONFIG_PATH)
+                .context("could not read configuration")?;
+        }
+        find_offending_processes(&config)
+            .context("error while examining offending processes")?;
+    }
+}
+
+pub struct UserInstructions {
+    user_name: String,
+    instructions: HashMap<Binary, Box<[config::Interval]>>,
+}
+impl UserInstructions {
+    pub fn new(user_name: String) -> Self {
+        UserInstructions {
+            user_name,
+            instructions: HashMap::new()
+        }
+    }
+}
+type PerUid = HashMap<Uid, UserInstructions>;
+
+pub fn init<P: AsRef<Path>>(path: &P) -> Result<PerUid, anyhow::Error> {
+    let reader = std::fs::File::open(path)
+        .with_context(|| format!("could not open file {}", path.as_ref().to_string_lossy()))?;
+    let config: config::Config = serde_yaml::from_reader(reader)
+        .with_context(|| format!("could not parse file {}", path.as_ref().to_string_lossy()))?;
+
+    let mut resolver = uid_resolver::Resolver::new();
+    let mut per_uid = HashMap::new();
+    for watch in config.watch {
+        let uid = resolver.resolve(&watch.user)?;
+        let per_binary = per_uid.entry(uid).or_insert_with(|| UserInstructions::new(watch.user));
+        per_binary.instructions.insert(watch.binary, watch.permitted.into_boxed_slice());
+    }
+    Ok(per_uid)
+}
+
+pub fn find_offending_processes(config: &PerUid) -> Result<(), anyhow::Error> {
+    if config.is_empty() {
+        return Ok(())
+    }
+
+    let now = TimeOfDay::from(chrono::Local::now());
+    let processes = procfs::process::all_processes()
+        .context("could not access /proc, is this a Linux machine")?;
+
+    for proc in processes {
+        // Examine process.
+        let proc = match proc {
+            Ok(p) => p,
+            Err(err) => {
+                log::warn!(target: "procfs", "could not access proc: {}", err);
+                continue;
+            }
+        };
+        let uid = match proc.uid() {
+            Ok(uid) => Uid(uid),
+            Err(err) => {
+                log::warn!(target: "procfs", "could not access proc uid for process {pid}: {}", err, pid=proc.pid);
+                continue;
+            }
+        };
+        let Some(per_binary) = config.get(&uid)
+        else {
+            // Nothing to watch for this user.
+            continue;
+        };
+        let exe = match proc.exe() {
+            Ok(exe) => Binary(exe),
+            Err(err@ProcError::PermissionDenied(_)) => {
+                log::debug!(target: "procfs", "could not access proc exe for process {pid}: {}", err, pid=proc.pid);
+                continue;
+            }
+            Err(err)  => {
+                log::warn!(target: "procfs", "could not access proc exe for process {pid}: {}", err, pid=proc.pid);
+                continue;
+            }
+        };
+        let Some(intervals) = per_binary.instructions.get(&exe)
+        else {
+            // Nothing to watch for this binary.
+            continue;
+        };
+        log::info!("found binary {} for user {}", exe, per_binary.user_name);
+        if let Some(duration) = intervals.iter().filter_map(|interval| interval.remaining(now)).next() {
+            // We're still in permitted territory.
+            log::info!("binary is still allowed at this time");
+            if duration < std::time::Duration::from_secs(300) {
+                // ...however, we're less than 5 minutes away from shutdown, so let's warn user!
+                let minutes = duration.as_secs() / 60;
+                if let Err(err) = notify(&per_binary.user_name, &format!("{} will quit in {} minutes", exe, minutes), notify::Urgency::Significant) {
+                    log::warn!(target: "notify", "failed to notify user {}: {:?}", per_binary.user_name, err)
+                }
+            }
+        } else {
+            log::info!("let's kill this binary");
+            // Time to kill the binary.
+            if let Err(err) = notify(&per_binary.user_name, &format!("{} is not permitted at this time, stopping it", exe), notify::Urgency::Significant) {
+                log::warn!(target: "notify", "failed to notify user {}: {:?}", per_binary.user_name, err)
+            }
+            if let Err(err) = kill_tree::blocking::kill_tree_with_config(proc.pid as u32, &kill_tree::Config { signal: "SIGKILL".to_string(), ..Default::default() }) {
+                log::warn!(target: "notify", "failed to kill process {:?}: {:?}", exe.0, err)
+            }
+            log::info!("binary killed");
+        }
+    }
     Ok(())
 }
