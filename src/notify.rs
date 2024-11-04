2223315b68e071a932ddae081691f47c65035b49
David Teller <david.teller@pasqal.com>
2024-11-04 18:13:17 +0100

    Well, it seems to work

--- src/notify.rs
+++ src/notify.rs
@@ -1,9 +1,11 @@
 use anyhow::Context;
+use log::info;
 
+#[allow(dead_code)]
 pub enum Urgency {
     Low,
     Significant,
-    Critical
+    Critical,
 }
 impl std::fmt::Display for Urgency {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -16,41 +18,21 @@ impl std::fmt::Display for Urgency {
     }
 }
 
-pub async fn notify(message: &str, urgency: Urgency, duration: std::time::Duration) -> Result<(), anyhow::Error> {
-    // Find dbus address.
-    let uid = unsafe { libc::geteuid() };
-    let dbus_address = format!("unix:path=/run/user/{uid}/bus");
-
-    // Find displays.
-    let who_regex = lazy_regex::regex!{"^([^ ]+).*\\(([^)]+)\\)$"m};
-    let who_output = tokio::process::Command::new("who")
+pub fn notify(
+    user: &str,
+    message: &str,
+    urgency: Urgency,
+) -> Result<(), anyhow::Error> {
+    info!("attempting to notify {user} of message {message}");
+    let _ = std::process::Command::new("systemd-run")
+        .arg("--user")
+        .arg(format!("--machine={user}@.host"))
+        .arg("notify-send")
+        .arg(format!("--urgency={urgency}"))
+        .arg("--app-name='Let\'s take a break'")
+        .arg(message)
         .output()
-        .await
-        .context("Failed to launch who")?;
-    let who_stdout = String::from_utf8(who_output.stdout)
-        .context("Invalid who output")?;
-    eprintln!("stdout {:?}", who_stdout);
-    for capture in who_regex.captures_iter(&who_stdout) {
-        let [user, display] = capture.extract::<2>().1;
-        eprintln!("user={}", user);
-        eprintln!("display={}", display);
-
-        let out = tokio::process::Command::new("sudo")
-            .args(["-u", user])
-            .arg(format!("DISPLAY={display}"))
-            .arg(format!("DBUS_SESSION_BUS_ADDRESS={dbus_address}"))
-            .arg("notify-send")
-            .arg("--app-name='Let\'s take a break'")
-            .arg(format!("--urgency={urgency}"))
-            .arg(format!("--expire-time={}", duration.as_millis()))
-            .arg(message)
-            .output()
-            .await
-            .context("Failed to launch sudo or notify")?;
-        let stdout = String::from_utf8_lossy(&out.stdout);
-        let stderr = String::from_utf8_lossy(&out.stderr);
-        eprintln!("out: {stdout}\nerr: {stderr}");
-    }
+        .context("Failed to launch systemd-run or notify-send")?;
 
     Ok(())
 }
---
+++ src/uid_resolver.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+use uucore::entries::{Locate, Passwd};
+
+use anyhow::Context;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Uid(pub u32);
+
+pub struct Resolver {
+    username_to_uid: HashMap<String, Uid>
+}
+
+impl Resolver {
+    pub fn new() -> Self {
+        Resolver {
+            username_to_uid: HashMap::new()
+        }
+    }
+    pub fn resolve(&mut self, name: &str) -> Result<Uid, anyhow::Error> {
+        if let Some(uid) = self.username_to_uid.get(name) {
+            return Ok(*uid)
+        }
+        let passwd = Passwd::locate(name)
+            .with_context(|| format!("could not find information for user {name}"))?;
+        let uid = Uid(passwd.uid);
+        self.username_to_uid.insert(name.to_string(), uid);
+        Ok(uid)
+    }
+}
